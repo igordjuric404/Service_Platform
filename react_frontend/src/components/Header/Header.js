8e173e0ee281378dfd5a4ed2d611051e144f2810
Igor Djuric <igordjuric404@gmail.com>
2024-12-15 11:12:07 +0100

    added header

---
+++ react_frontend/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- react_frontend/src/routes/AppRoutes.js
+++ react_frontend/src/routes/AppRoutes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from '../components/Header/Header'; 
 import HomePage from '../pages/HomePage/HomePage';
 import Register from '../pages/Register/Register';
+import Login from '../pages/Login/Login'; 
 
 function AppRoutes() {
   return (
     <Router>
+      <Header /> 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
